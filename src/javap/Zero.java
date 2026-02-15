68e68e91788ec213cf729a5a85fa4d95516162a8
Sripadhmapiriya <sripadhmapiriya12@gmail.com>
2026-02-15 15:29:29 +0530

    Add initial implementation of Zero class with main method

---
+++ src/javap/Zero.java
@@ -0,0 +1,20 @@
+package.japap;
+public class Zero{
+    public static void main(String[]args){
+        int num[] = {0,1,0,3,12};
+        int ind = 0;
+        for ( int i = 0; i < num.length-1; i++){
+            if( num[i] != 0){
+                ind += num[i];
+                index ++;
+            }
+        }
+        while( ind < num.length){
+            ind[i] = 0;
+            break;
+        }
+        for ( x : num){
+            System.out.println( x + " ");
+        }
+    }
+}
